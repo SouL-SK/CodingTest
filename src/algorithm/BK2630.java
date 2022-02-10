e644d4425576640fa2b107abed8d302949dd5460
SOLokill <shuai1593@gmail.com>
2022-02-10 20:06:01 +0900

    upload

--- src/algorithm/BK2630.java
+++ src/algorithm/BK2630.java
@@ -1,28 +1,37 @@
 package algorithm;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+
 
 /**
  * 백준 
  * 2630번 색종이 만들기
  * @author shuai
- *
+ * Nuberformat error
  */
 public class BK2630 {
-	static Scanner sc = new Scanner(System.in);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static int n;
-	static int[][] list = new int[n][n];
+	static int[][] list;
+	static int[] count = new int[2];
 
-	public static void main(String[] args) throws IOException {
-		n = sc.nextInt();
+	public static void main(String[] args) throws IOException, NumberFormatException {
+		n = Integer.parseInt(br.readLine());
+		list = new int[n][n];
+		String line = "";
+		while (true) {
+			line = br.readLine();
+			if (line == null) break;
+			
+		}
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				list[i][j] = sc.nextInt();
+				list[i][j] = Integer.parseInt(br.readLine());
 			}
 		}
 		Solution(n, 0, 0);
-		Solution(n/2, );
 	}
 
 	public static boolean Solution(int n, int x, int y) {
@@ -31,11 +40,15 @@ public class BK2630 {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				if (list[i][j] != val) {
+					Solution(n/2, x, y);
+					Solution(n/2, x, y+n/2);
+					Solution(n/2, x+n/2, y);
+					Solution(n/2, x+n/2, y+n/2);
 					return false;
 				}
 			}
 		}
-
+		count[list[x][y]]++;
 		return true;
 	}
 }
