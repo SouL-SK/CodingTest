d4d8c2454bf0c56b201d939880913cf94bc41f97
SOLokill <shuai1593@gmail.com>
2022-02-24 23:12:42 +0900

    220224 update

---
+++ src/algorithm/BK10026.java
@@ -0,0 +1,38 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 10026번 적록색약 문제
+ * 
+ * @author shuai
+ *
+ */
+public class BK10026 {
+
+	public static void main(String[] args) throws IOException, NoSuchElementException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = br.read();
+		String s = br.readLine();
+
+		String[][] list = new String[n][n];
+		StringTokenizer st = new StringTokenizer(s, " ");
+		for (int i = 0; i < n; i++) {
+			int in = 0;
+			while (st.hasMoreElements()) {
+				list[in++][n] = st.nextToken();
+
+			}
+		}
+		
+		for (int i = 0; i < n; i++) {
+			
+		}
+		
+	}
+
+}
---
+++ src/algorithm/BK2667.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+/**
+ * 백준 2667번
+ * 단지번호붙이기
+ * @author shuai
+ *
+ */
+public class BK2667 {
+	
+}
