d364b10fd64c6f136f14cf734692308f9d96dc85
SOLokill <shuai1593@gmail.com>
2022-04-28 03:33:34 +0900

    BK10870

---
+++ src/algorithm/BK10870.java
@@ -0,0 +1,19 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class BK10870 {
+	static Scanner sc = new Scanner(System.in);
+	static int n = sc.nextInt();
+	static int result = 0;
+
+	public static void main(String[] args) {
+		Solution(n);
+	}
+
+	static int Solution(int n) {
+		result = 0; 
+//				구현할 부분을 구글링 해보자
+		return result;
+	}
+}
