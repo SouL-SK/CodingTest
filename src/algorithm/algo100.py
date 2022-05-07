560c0aac78fd5c3f155063f06a2c33b37d16e2a4
SOLokill <shuai1593@gmail.com>
2022-05-07 20:49:47 +0900

    update python toturial

--- src/algorithm/algo100.py
+++ src/algorithm/algo100.py
@@ -1,11 +1,5 @@
-n = int(input())
-if n == 12 or n == 1 or n == 2:
-    print("winter")
-elif n // 3 == 1:
-    print("spring")
-elif n // 6 == 1:
-    print("summer")
-elif n // 9 == 1 and n != 12:
-    print("fall")
-else:
-    print("None")
+n = 1      #처음 조건 검사를 통과하기 위해 0 아닌 값을 임의로 저장
+while n!=0 :
+  n = int(input())
+  if n!=0 :
+    print(n)
