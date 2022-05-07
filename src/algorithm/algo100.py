9759039b1ed591a90be3eb7a65c3127096484c9c
SOLokill <shuai1593@gmail.com>
2022-05-07 23:47:50 +0900

    update pyTuto_6080

--- src/algorithm/algo100.py
+++ src/algorithm/algo100.py
@@ -1,7 +1,5 @@
-n = int(input())
-sum = 0
-count = 0
-while sum < n:
-    sum += count
-    count += 1
-print(count-1)
+n, m= input().split()
+print(n, m, end='\n')
+for i in range(1, n+1) :
+  for j in range(1, m+1) :
+    print(i, j)
