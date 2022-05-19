a7a667e23ee1b07784af0379940a1352376c4cbd
SOLokill <shuai1593@gmail.com>
2022-05-19 23:49:43 +0900

    update

--- src/algorithm/BK10870.java
+++ src/algorithm/BK10870.java
@@ -7,21 +7,31 @@ import java.util.Scanner;
  * 마지막 항을 구하면 된다.
  */
 public class BK10870 {
-	static Scanner sc = new Scanner(System.in);
-	static int n = sc.nextInt();
-	static int result = 0;
-	static int[] temp = new int[n];
-//	저장 용량을 설정하지 않으면 생기는 오류 : Variable must provide either dimension expressions or an array initializer
 
-	int Solution(int n) {
-		if (n == 0) {
-			return 0;
-		}
-		if (n == 1) {
-			return 1;
-		}
-		return Solution(n - 1) + Solution(n - 2);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+//		int result = 0;	필요없는 줄
+//		int[] temp = new int[n]; 필요없는 줄
+//	저장 용량을 설정하지 않으면 생기는 오류 : Variable must provide either dimension expressions or an array initializer
+		System.out.println(Solution(n));
+
+		sc.close();
+
 	}
-	
-	
+
+	static int Solution(int n) {
+//		if (n == 0) {
+//			return 0;
+//		}
+//		if (n == 1) {
+//			return 1;
+//		}
+		if (n <= 1) {
+			return n;
+		} else {
+			return Solution(n - 1) + Solution(n - 2);
+		}
+	}
+
 }
---
+++ src/algorithm/BK7568.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import java.util.HashMap;
+
+public class BK7568 {
+	int kg, cm = 0;
+	HashMap<Integer, Integer> person = new HashMap<Integer, Integer>();
+
+	public static void main(String[] args) {
+		
+	}
+}
