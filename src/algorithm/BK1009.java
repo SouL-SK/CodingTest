df7808f6d8f22dd39872f3e573b18e328ca902c8
SOLokill <shuai1593@gmail.com>
2022-05-23 23:58:02 +0900

    update 220523

---
+++ src/algorithm/BK1009.java
@@ -0,0 +1,16 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BK1009 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+//		ArrayList<Integer> list = new ArrayList<Integer>();
+//		list.add(sc.nextInt());
+		
+		int result = T%10;
+	}
+}
