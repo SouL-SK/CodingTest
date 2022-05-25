7d62ee71e77fd1eebf82bfeac4e1608eb621ad32
SOLokill <shuai1593@gmail.com>
2022-05-25 22:22:54 +0900

    update 220525

---
+++ src/algorithm/BK2163.java
@@ -0,0 +1,23 @@
+package algorithm;
+
+/**
+ * 백준 2163번 초콜릿 자르기
+ * 
+ * 조건
+ * - 초콜릿 크기는 N * M. 쪼개어 N * M 개의 조각을 만들 수 있다.
+ * - 초콜릿을 쪼개면 두 개의 조각으로 나눠지고, 이 중에서 초콜릿 조각을 하나 들고 또 쪼갠다.
+ * - 1 * 1 크기의 초콜릿으로 쪼개기 위한 최소 쪼개기 횟수를 구하는 프로그램을 작성하라.
+ * 
+ * 헷갈린 부분
+ * - 반으로 나눈 뒤에 또 반으로 나누면서 조각 마다 횟수를 세야하는건지? 
+ * - 
+ *
+ */
+import java.util.Scanner;
+
+public class BK2163 {
+	public static void main(String[] agrs) {
+		Scanner sc = new Scanner(System.in);
+		
+	}
+}
