8ac744b1e14a5356441dcd73f9f2e01962bd11b1
SOLokill <shuai1593@gmail.com>
2022-07-01 23:12:37 +0900

    20220701

---
+++ src/algorithm_java/BK3009.java
@@ -0,0 +1,23 @@
+package algorithm_java;
+/*
+    problem : beakjoon NO.3009
+    task : 세 점이 주어졌을 때, 축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오
+    think :
+        - 세 점의 좌표가 한 줄에 하나씩 주어진다. 좌표는 1보다 크거나 같고, 1000보다 작거나 같은 정수이다.
+        - 직사각형의 네 번째 점의 좌표를 출력한다
+        - x좌표, y좌표를 받아 위치를 저장하고 비어있는 공간을 찾아 수평 위치에 해당하는 x, y 자표를 가져온다.
+
+ */
+import java.util.Scanner;
+
+public class BK3009 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] addList = new int[4];
+        for(int i = 0; i < 3;i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+        }
+    }
+}
