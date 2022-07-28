614302ee2bda13cd04a3d1c92b841c592fa3fd01
SOLokill <shuai1593@gmail.com>
2022-07-28 22:48:31 +0900

    2020728

--- src/algorithm_java/BK6603.java
+++ src/algorithm_java/BK6603.java
@@ -6,8 +6,11 @@ package algorithm_java;
         - 입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 한 줄로 이루어져 있다. 첫 번째 수는 k (6 < k < 13)이고, 다음 k개 수는 집합 S에 포함되는 수이다.
         S의 원소는 오름차순으로 주어진다. 입력의 마지막 줄에는 0이 하나 주어진다.
         - 각 테스트 케이스마다 수를 고르는 모든 방법을 출력한다. 이때, 사전 순으로 출력한다. 각 테스트 케이스 사이에는 빈 줄을 하나 출력한다.
+        - 부분 집합들을 구해야 한다.
+        - 백트래킹을 활용한 dfs 이다.
     explain :
     conclusion :
+        elapsed time : 376 ms, memory : 20512 kb, time complexity :
  */
 
 import java.io.BufferedReader;
@@ -17,22 +20,48 @@ import java.util.StringTokenizer;
 
 public class BK6603 {
 
-    static int N, K;
-    static int[] list;
+    static int K;
+    static int[] S;
+    static boolean[] visit;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            N = Integer.parseInt(st.nextToken());
-            if (N == 0) {
+            K = Integer.parseInt(st.nextToken());
+            if (K == 0) {
                 break;
             }
-            list = new int[N];
-            for (int i = 0; i < N; i++) {
-                list[i] = Integer.parseInt(st.nextToken());
+            S = new int[K];
+            visit = new boolean[K];
+            for (int i = 0; i < K; i++) {
+                S[i] = Integer.parseInt(st.nextToken());
             }
+//            for (int i = 0; i < list.length; i++) {
+//                System.out.print(list[i] + " ");
+//            }
+//            System.out.println();
+            dfs(0, 0);
+            System.out.println();
         }
 
     }
+
+    public static void dfs(int start, int depth) {
+        if (depth == 6) {
+            for (int i = 0; i < K; i++) {
+                if (visit[i]) {
+                    System.out.print(S[i] + " ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = start; i < K; i++) {
+            visit[i] = true;
+            dfs(i + 1, depth + 1);
+            visit[i] = false;
+        }
+    }
+
 }
