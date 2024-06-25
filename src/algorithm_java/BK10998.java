7cf50703b71dca52c46478018159d972638272cb
SouL-SK <shuai1593@gmail.com>
2024-06-25 22:52:14 +0900

    20240625

---
+++ src/algorithm_java/BK10998.java
@@ -0,0 +1,21 @@
+package algorithm_java;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * baekjoon A×B
+ * 두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+ *
+ * input:
+ * 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * output:
+ * 첫째 줄에 A×B를 출력한다
+ */
+public class BK10998 {
+
+        public static void main(String[] args) {
+
+        }
+
+}
